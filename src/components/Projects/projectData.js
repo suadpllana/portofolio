f57a7464bfd5d3a4b3ba214f30fadfbe2c75b686
suadpllana <suadpllana12@gmail.com>
2025-06-28 21:55:01 +0200

    first commit

--- src/components/Projects/projectData.js
+++ src/components/Projects/projectData.js
@@ -1,19 +1,28 @@
 import { projectImages , thumbnailsImages} from "../../images";
 
 export  const projectData = [
+
    {
-      title: "Football",
-      tools: "React",
-      link: "https://football-app1.netlify.app",
-      image: projectImages.football1,
-      thumbnails: [projectImages.football1,thumbnailsImages.football2,thumbnailsImages.football3,thumbnailsImages.football4,thumbnailsImages.football5,thumbnailsImages.football6,thumbnailsImages.football7,thumbnailsImages.football8,thumbnailsImages.football9,thumbnailsImages.football10,thumbnailsImages.football11,thumbnailsImages.football12,thumbnailsImages.football13,thumbnailsImages.football14,thumbnailsImages.football15,thumbnailsImages.football16,thumbnailsImages.football17,thumbnailsImages.football18,thumbnailsImages.football19,thumbnailsImages.football20,thumbnailsImages.football21]
+      title: "PSG",
+      tools: "React, Node.js, MongoDB",
+      link: "https://psg-news.netlify.app",
+      image: projectImages.psg,
+      thumbnails: [projectImages.psg, thumbnailsImages.psg1, thumbnailsImages.psg2,thumbnailsImages.psg3,thumbnailsImages.psg4,thumbnailsImages.psg5,thumbnailsImages.psg6,thumbnailsImages.psg7,thumbnailsImages.psg8,thumbnailsImages.psg9,thumbnailsImages.psg10,]
     },
+ 
     {
       title: "Library",
       tools: "React",
       link: "https://suadpllana.github.io/library",
       image: projectImages.books,
       thumbnails: [projectImages.books, thumbnailsImages.books1,thumbnailsImages.books2,thumbnailsImages.books3,thumbnailsImages.books4,thumbnailsImages.books5,thumbnailsImages.books6,thumbnailsImages.books7,thumbnailsImages.books8,thumbnailsImages.books9,]
+    },
+      {
+      title: "Football",
+      tools: "React",
+      link: "https://football-app1.netlify.app",
+      image: projectImages.football1,
+      thumbnails: [projectImages.football1,thumbnailsImages.football2,thumbnailsImages.football3,thumbnailsImages.football4,thumbnailsImages.football5,thumbnailsImages.football6,thumbnailsImages.football7,thumbnailsImages.football8,thumbnailsImages.football9,thumbnailsImages.football10,thumbnailsImages.football11,thumbnailsImages.football12,thumbnailsImages.football13,thumbnailsImages.football14,thumbnailsImages.football15,thumbnailsImages.football16,thumbnailsImages.football17,thumbnailsImages.football18,thumbnailsImages.football19,thumbnailsImages.football20,thumbnailsImages.football21]
     },
     {
       title: "Movies",
@@ -22,14 +31,6 @@ export  const projectData = [
       image: projectImages.movies,
       thumbnails: [projectImages.movies, thumbnailsImages.movies1, thumbnailsImages.movies2,thumbnailsImages.movies3,thumbnailsImages.movies4,thumbnailsImages.movies5,thumbnailsImages.movies6,thumbnailsImages.movies7,thumbnailsImages.movies8,thumbnailsImages.movies9,thumbnailsImages.movies10]
     },
-     {
-      title: "PSG",
-      tools: "React, Node.js, MongoDB",
-      link: "https://psg-news.netlify.app",
-      image: projectImages.psg,
-      thumbnails: [projectImages.psg, thumbnailsImages.psg1, thumbnailsImages.psg2,thumbnailsImages.psg3,thumbnailsImages.psg4,thumbnailsImages.psg5,thumbnailsImages.psg6,thumbnailsImages.psg7,thumbnailsImages.psg8,thumbnailsImages.psg9,thumbnailsImages.psg10,]
-    },
-   
     {
       title: "Anime",
       tools: "React,Sass",
@@ -44,8 +45,5 @@ export  const projectData = [
       link: "https://suadpllana.github.io/crypto-prices/",
       image: projectImages.crypto,
       thumbnails: [projectImages.crypto,thumbnailsImages.crypto1,thumbnailsImages.crypto2,thumbnailsImages.crypto3,thumbnailsImages.crypto4,thumbnailsImages.crypto5,thumbnailsImages.crypto6,thumbnailsImages.crypto7]
-    },
-
- 
-  
+    }
   ]
